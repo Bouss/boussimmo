5964ebaf32b135172018301ddff1c8749399aff2
bouss <florian.bousseaud@guestapp.me>
2020-05-02 18:50:17 +0200

    Stored static data in root 'data' folder

--- src/Enum/City.php
+++ src/Enum/City.php
@@ -4,6 +4,15 @@ namespace App\Enum;
 
 class City extends AbstractEnum
 {
+    public const PARIS = 'paris';
+    public const MARSEILLE = 'marseille';
+    public const LYON = 'lyon';
+    public const TOULOUSE = 'toulouse';
+    public const NICE = 'nice';
     public const NANTES = 'nantes';
+    public const MONTPELLIER = 'montpellier';
+    public const STRASBOURG = 'strasbourg';
+    public const BORDEAUX = 'bordeaux';
+    public const LILLE = 'lille';
     public const RENNES = 'rennes';
 }
