045bcfa92f08f218801ec35251a4db2f712e3916
bouss <florian.bousseaud@guestapp.me>
2020-08-20 12:16:35 +0200

    Fixed the creation of the user table for PostgreSQL

--- migrations/Version20200126003702.php
+++ migrations/Version20200126003702.php
@@ -21,7 +21,7 @@ final class Version20200126003702 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         if ($this->connection->getDatabasePlatform()->getName() === 'postgresql') {
-            $this->addSql('CREATE TABLE "user" (id SERIAL, email varchar(180) NOT NULL, google_id varchar(255) NOT NULL, access_token varchar(255) NOT NULL, profile_image varchar(255) DEFAULT NULL, roles json NOT NULL, CONSTRAINT UNIQ_8D93D649E7927C74 UNIQUE(email), CONSTRAINT UNIQ_8D93D64976F5C865 UNIQUE(google_id), PRIMARY KEY(id)) WITH ENCODING \'UTF8\' LC_COLLATE = \'en_US.UTF-8\' LC_CTYPE = \'en_US.UTF-8\' TEMPLATE = template0');
+            $this->addSql('CREATE TABLE "user" (id SERIAL, email varchar(180) NOT NULL, google_id varchar(255) NOT NULL, access_token varchar(255) NOT NULL, profile_image varchar(255) DEFAULT NULL, roles json NOT NULL, CONSTRAINT UNIQ_8D93D649E7927C74 UNIQUE(email), CONSTRAINT UNIQ_8D93D64976F5C865 UNIQUE(google_id), PRIMARY KEY(id))');
         } else {
             $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, google_id VARCHAR(255) NOT NULL, access_token VARCHAR(255) NOT NULL, profile_image VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D64976F5C865 (google_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         }
