e16b8472017c5d89be84b9dae33a84e9511594d0
bouss <florian.bousseaud@guestapp.me>
2020-08-20 10:27:48 +0200

    Make the creation of the 'user' table work for both MySQL and PostgreSQL

--- migrations/Version20200126003702.php
+++ migrations/Version20200126003702.php
@@ -20,7 +20,15 @@ final class Version20200126003702 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE "user" (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, google_id VARCHAR(255) NOT NULL, access_token VARCHAR(255) NOT NULL, profile_image VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D64976F5C865 (google_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE "user" DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
+        if ($this->connection->getDatabasePlatform()->getName() === 'postgresql') {
+            $this->addSql('ALTER TABLE "user" ADD id SERIAL');
+        } else {
+            $this->addSql('ALTER TABLE "user" ADD id INT AUTO_INCREMENT NOT NULL');
+        }
+
+        $this->addSql('ALTER TABLE "user" ADD email VARCHAR(180) NOT NULL, ADD google_id VARCHAR(255) NOT NULL, ADD access_token VARCHAR(255) NOT NULL, ADD profile_image VARCHAR(255) DEFAULT NULL, ADD roles JSON NOT NULL, ADD UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), ADD UNIQUE INDEX UNIQ_8D93D64976F5C865 (google_id), ADD PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
