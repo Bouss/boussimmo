8948d361507a06e907aa2e1d0b6ef3fefb939ba5
bouss <florian.bousseaud@guestapp.me>
2021-02-23 10:46:55 +0100

    Fixed BienIci new-build detection

--- src/Parser/BienIciParser.php
+++ src/Parser/BienIciParser.php
@@ -9,8 +9,10 @@ class BienIciParser extends AbstractParser
 {
     protected const PROVIDER = Provider::BIENICI;
 
-    protected const SELECTOR_AD_WRAPPER  = '.realEstateAd';
-    protected const SELECTOR_LOCATION    = '.realEstateAdAddress a';
+    protected const SELECTOR_AD_WRAPPER    = '.realEstateAd';
+    protected const SELECTOR_LOCATION      = '.realEstateAdAddress a';
+    protected const SELECTOR_BUILDING_NAME = '.realEstateAdTitle strong';
+    protected const SELECTOR_DESCRIPTION   = '.newProperty';
 
     /**
      * {@inheritDoc}
@@ -22,5 +24,20 @@ class BienIciParser extends AbstractParser
             ['600x370', 'width=600&height=370'],
             parent::parsePhoto($crawler)
         );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function parseBuildingName(Crawler $crawler): ?string
+    {
+        $title = parent::parseBuildingName($crawler);
+
+        // E.g.: "Les Jardins d'Antoine (1 à 4 pièces, 32 à 78 m²)" (vs "Appartement 3 pièces 71 m²")
+        if (1 === preg_match('/(.+) \(.+\)/', $title, $matches)) {
+            return $matches[1];
+        }
+
+        return null;
     }
 }
